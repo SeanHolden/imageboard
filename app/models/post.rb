b95d2e4a4b8a725e60dbe85a13547c63b61d4bdc
Sean Holden <Sean.Holden@o2.com>
2012-08-06 15:53:35 +0100

    edited the layout slightly

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   attr_accessible :content, :username, :photo
 
-  has_attached_file :photo, :styles => { :small => "150x150" }
+  has_attached_file :photo, :styles => { :small => "150x150" },
+                   # :url => "/system/:class/:attachment/:id_partition/:style/:filename",
+                   # :path => ":rails_root/public:url"
+                    :url => "/assets/:class/:attachment/:id/:style/:filename",
+                    :path => ":rails_root/public:url"
+                   
 
 end
